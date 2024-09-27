9463122908dd5d43ca1b05179fc7ae1b73ff27de
JulioFertry <juliofertry@gmail.com>
2024-09-27 21:28:21 +0200

    Segunda y Tercera parte

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,23 @@ fun main() {
 
     //Instancia un lector de archivos
     val reader = FileReader()
+    val writer = Output()
 
     // Crea las rutas necesarias
     val root = Path.of("src")
     val csvFilePath = root.resolve("main").resolve("resources").resolve("calificaciones.csv")
 
-    val info = reader.getSubjectInfo(csvFilePath)
+    // Lee el fichero y lo transforma a Lista
+    var info = reader.getSubjectInfo(csvFilePath)
+
+    info = writer.addFinalGrades(info)
     info.forEach { println(it) }
 
+    val (passed, failed) = writer.createPassedLists(info)
+
+    println("Aprobados:")
+    passed.forEach { println(it) }
+
+    println("\nSuspensos:")
+    failed.forEach { println(it) }
 }
--- src/main/kotlin/Output.kt
+++ src/main/kotlin/Output.kt
@@ -1,5 +1,52 @@
 class Output {
 
-    val gradeCalculator = GradeCalculator()
+    private val gradeCalculator = GradeCalculator()
+
+
+    /** Recibe la información de los estudiantes sin notas finales y devuelve la información con las notas finales
+     *
+     * @param studentsInfo Información de los estudiantes sin notas finales
+     *
+     * @return Información de los estudiantes con notas finales
+     */
+    fun addFinalGrades(studentsInfo: List<Map<String, List<Float>>>): List<Map<String, List<Float>>> {
+        val studentsCompletedInfo = mutableListOf<Map<String, List<Float>>>()
+
+        studentsInfo.forEach { dictionary ->
+            val student = dictionary.keys.first()
+            val grades = dictionary.values.first().toMutableList()
+
+            val finalGrade = gradeCalculator.calculateFinalGrade(dictionary)
+            grades.add(finalGrade)
+
+            val studentsUpdatedInfo = mapOf(student to grades)
+            studentsCompletedInfo.add(studentsUpdatedInfo)
+        }
+
+        return studentsCompletedInfo
+    }
+
+
+    /** Con la información de los estudiantes completa crea una lista de alumnos aprobados y otra de suspensos
+     *
+     * @param studentsInfo Información de los estudiantes completa
+     *
+     * @return Listas de aprobados y de suspensos
+     */
+    fun createPassedLists(studentsInfo: List<Map<String, List<Float>>>): Pair<List<String>, List<String>> {
+        val passed = mutableListOf<String>()
+        val failed = mutableListOf<String>()
+
+        studentsInfo.forEach { dictionary ->
+            val studentName = dictionary.keys.first()
+            if (gradeCalculator.hasPassed(dictionary)) {
+                passed.add(studentName)
+            } else {
+                failed.add(studentName)
+            }
+        }
+
+        return Pair(passed, failed)
+    }
 
 }
